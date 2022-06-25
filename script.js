4547f60bc245589550a2a0c97d016fbb098d0cc2
vuvu123 <vuvu123@gmail.com>
2022-06-25 14:42:42 -0700

    Refactor, create grid using CSS grid

--- script.js
+++ script.js
@@ -1,51 +1,15 @@
-const container = document.querySelector('.container');
+const grid = document.querySelector('#grid');
 
-function createDivs(size) {
-  for (let i = 0; i < size; i++) {
-    const rowDiv = document.createElement('div');
-    rowDiv.className = 'row-container';
-    container.appendChild(rowDiv);
-    for (let j = 0; j < size; j++) {
-      const colDiv = document.createElement('div');
-      colDiv.className = 'grid-div';
-      rowDiv.appendChild(colDiv);
-    }
+function generateGrid(size) {
+  // grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  // grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size ** 2; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add("grid-item");
+    grid.appendChild(gridItem);
   }
 }
 
-createDivs(16);
-
-// Add event listener when mouse hovers over div
-// When mouse hovers over, add class "hovered" to element
-const allGridDivs = document.querySelectorAll('.grid-div');
-
-allGridDivs.forEach((div) => {
-  div.addEventListener('mouseover', () => {
-    div.className = 'grid-div hovered';
-  });
-});
-
-// Add clear button to clear color from grid ()
-const clearButton = document.querySelector('#clear');
-
-clearButton.addEventListener('click', () => {
-  allGridDivs.forEach((div) => {
-    div.className = 'grid-div';
-  });
-});
-
-
-// TODO: Slider Bar 
-// Add input element, type=range, values: 10-60
-const sizeRange = document.querySelector('#size');
-const sizeDisplay = document.querySelector('#size-display');
-
-sizeRange.addEventListener('input', () => {
-  sizeDisplay.textContent = `${sizeRange.value}x${sizeRange.value}`;
-});
-
-
-
-
-
-// Add random color
+generateGrid(16);
