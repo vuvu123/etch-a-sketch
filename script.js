f347122f1126a6c8e46943c41846caa4ff2ecc86
vuvu123 <vuvu123@gmail.com>
2022-06-24 20:10:58 -0700

    Add mouse over div color functionality

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.querySelector('.container');
 
 function createDivs() {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 16; i++) {
     const rowDiv = document.createElement('div');
-    rowDiv.className = 'grid-div row-container';
+    rowDiv.className = 'row-container';
     container.appendChild(rowDiv);
-    for (let j = 0; j < 5; j++) {
+    for (let j = 0; j < 16; j++) {
       const colDiv = document.createElement('div');
       colDiv.className = 'grid-div';
       rowDiv.appendChild(colDiv);
@@ -14,3 +14,14 @@ function createDivs() {
 }
 
 createDivs();
+
+// Add event listener when mouse hovers over div
+// When mouse hovers over, add class "hovered" to element
+const allGridDivs = document.querySelectorAll('.grid-div');
+
+allGridDivs.forEach((div) => {
+  div.addEventListener('mouseover', () => {
+    div.className = 'grid-div hovered';
+  });
+});
+
