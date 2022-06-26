2dadcc1aaf2d506b4fde34b7c3510be6749ac2be
vuvu123 <vuvu123@gmail.com>
2022-06-25 17:26:03 -0700

    added clear button function

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const grid = document.querySelector('#grid');
+const clearBtn = document.querySelector('#clearBtn');
 
 function generateGrid(size) {
-  // grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  // grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   grid.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
 
   for (let i = 0; i < size ** 2; i++) {
     const gridItem = document.createElement('div');
     gridItem.classList.add("grid-item");
+    gridItem.addEventListener('mouseover', changeColor);
     grid.appendChild(gridItem);
   }
 }
 
 generateGrid(16);
+
+function changeColor(e) {
+  e.target.style.backgroundColor = 'blue';
+}
+
+function clearGrid() {
+  const gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach((gridItem) => {
+    grid.removeChild(gridItem);
+  });
+}
+
+clearBtn.addEventListener('click', () => {
+  clearGrid();
+  generateGrid(16);
+});
